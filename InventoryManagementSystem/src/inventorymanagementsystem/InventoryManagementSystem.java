064b34878588347f3b7d6f7f922868fffdec70f8
Patrick Larocque <patricklarocque1@gmail.com>
2022-08-24 21:21:42 -0400

    Inventory Management System. A core Java project. GUI completed. Database via Apache Derby. CRUD functionality.

---
+++ InventoryManagementSystem/src/inventorymanagementsystem/InventoryManagementSystem.java
@@ -0,0 +1,17 @@
+package inventorymanagementsystem;
+
+/**
+ *
+ * @author Patrick Larocque
+ */
+
+public class InventoryManagementSystem {
+    
+    /**
+    * @param args the command line arguments
+    */
+    
+    public static void main(String args[]) {
+
+    }
+}
